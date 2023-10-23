bcda4762be5ae45cffb4baa6f232d25137263346
David Ohlemacher <dohlemacher@infinidat.com>
2023-10-23 13:12:07 -0400

    psdev-1124: Add more to users repo module.
    DCO 1.1 Signed-off-by: David Ohlemacher <dohlemacher@infinidat.com>

--- plugins/modules/infini_users_repository.py
+++ plugins/modules/infini_users_repository.py
@@ -19,6 +19,13 @@ description:
     - This module creates, deletes or modifies respositories of users that can log on to an Infinibox.
 author: David Ohlemacher (@ohlemacher)
 options:
+  ad_domain_name:
+    required: false
+    default: None
+  ad_auto_discover_servers:
+    required: false
+    choices: [True, False]
+    default: True
   bind_username:
     description:
       - The bind username
@@ -145,6 +152,78 @@ except ImportError:
 HAS_CAPACITY = False
 
 
+@api_wrapper
+def get_users_repository(module, disable_fail=False):
+    """
+    Find and return users repository information
+    Use disable_fail when we are looking for an user repository
+    and it may or may not exist and neither case is an error.
+    """
+    system = get_system(module)
+    name = module.params["name"]
+
+    path = f"config/ldap?name={name}"
+    repo = system.api.get(path=path)
+
+    if repo:
+        result = repo.get_result()
+        if not disable_fail and not result:
+            msg = f"Users repository {name} not found. Cannot stat."
+            module.fail_json(msg=msg)
+        return result
+    elif disable_fail:
+        return None
+
+    msg = f"Users repository {name} not found. Cannot stat."
+    module.fail_json(msg=msg)
+
+
+def handle_stat(module):
+    """Return users repository stat"""
+    name = module.params['name']
+    repos = get_users_repository(module)
+    try:
+        assert len(repos) == 1
+    except AssertionError:
+        msg = f"Users repository {name} not found in repository list {repos}. Cannot stat."
+        module.fail_json(msg=msg)
+    result = repos[0]
+    result["repository_id"] = result.pop("id")  # Rename id to repository_id
+    result["changed"] = False
+    module.exit_json(**result)
+
+
+def handle_present(module):
+    """Make users repository present"""
+    name = module.params['name']
+    changed = False
+    msg = f"Users repository {name} unchanged"
+    if not module.check_mode:
+        old_users_repository = get_users_repository(module, disable_fail=True)
+        put_users_repository(module)
+        new_users_repository = get_users_repository(module)
+        changed = new_users_repository != old_users_repository
+        if changed:
+            msg = f"Users repository {name} changed"
+        else:
+            msg = f"Users repository {name} unchanged since the value is the same as the existing users repository"
+    module.exit_json(changed=changed, msg=msg)
+
+
+def handle_absent(module):
+    """Make users repository absent"""
+    name = module.params['name']
+    msg = f"Users repository {name} unchanged"
+    changed = False
+    if not module.check_mode:
+        changed = delete_users_repository(module)
+        if changed:
+            msg = f"Users repository {name} removed"
+        else:
+            msg = f"Users repository {name} did not exist so no removal was necessary"
+    module.exit_json(changed=changed, msg=msg)
+
+
 def execute_state(module):
     """Determine which state function to execute and do so"""
     state = module.params["state"]
@@ -166,6 +245,7 @@ def check_options(module):
     """Verify module options are sane"""
     bind_username = module.params["bind_username"]
     bind_password = module.params["bind_password"]
+    ad_domain_name = module.params["ad_domain_name"]
     name = module.params["name"]
     port = module.params["port"]
     repository_type = module.params["repository_type"]
@@ -193,11 +273,13 @@ def main():
 
     argument_spec.update(
         {
-            "bind_password": {"required": True, "default": None},
-            "bind_username": {"required": True},
+            "ad_domain_name": {"required": False, "default": None},
+            "ad_auto_discover_servers": {"required": False, "choices": [True, False], "default": True},
+            "bind_password": {"required": False, "default": None, "no_log": True},
+            "bind_username": {"required": False, "default": None},
             "name": {"required": True},
             "port": {"required": False, "type": int, "default": 636},
-            "repository_type": {"required": True, "choices": [True, False]},
+            "repository_type": {"required": False, "choices": ["LDAP", "AD"], "default": "LDAP"},
             "schema_group_attribute": {"required": False, "default": "cn"},
             "schema_group_basedn": {"required": False, "default": None},
             "schema_member_attribute": {"required": False, "default": "memberof"},
